9aa5bb2a3b58e41d6b84e27426cd298940e74587
Krishna Kant <kkant2043@gmail.com>
2026-05-17 18:28:37 +0530

    update lenis dependency to latest version

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { useEffect } from "react";
-import Lenis from "@studio-freight/lenis";
+import Lenis from "lenis";
 import CinematicHero from "@/components/CinematicHero";
 import PortfolioGrid from "@/components/PortfolioGrid";
 
