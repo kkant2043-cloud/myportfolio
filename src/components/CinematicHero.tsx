5c996628994b0e4f24dc1176c685e39180f51d4d
Krishna Kant <kkant2043@gmail.com>
2026-05-17 18:19:59 +0530

    fix maath import error

--- src/components/CinematicHero.tsx
+++ src/components/CinematicHero.tsx
@@ -3,7 +3,7 @@ import { useRef } from "react";
 import { Canvas, useFrame } from "@react-three/fiber";
 import { Points, PointMaterial } from "@react-three/drei";
 import { motion } from "framer-motion";
-import * as random from "maath/random/dist/maath-random.esm";
+import * as random from "maath/random";
 
 function ParticleSwarm() {
   const ref = useRef<any>();
